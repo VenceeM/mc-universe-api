a75c426552dcf033a41925d96e2154746ce2b0e5
officialvencee <officialvencee.manansala@gmail.com>
2022-01-10 22:27:15 +0800

    adding test for getting user profile

--- test/profile.test.js
+++ test/profile.test.js
@@ -1,6 +1,6 @@
 const app = require('../src/app.js')
 const request = require('supertest')
-const { userOne, userOneId, initialSetup } = require('./fixture/default.js')
+const { userOne, userOneId, initialSetup, profileOne, profileOneId, userTwo, userTwoId } = require('./fixture/default.js')
 
 beforeEach(initialSetup)
 
@@ -8,7 +8,7 @@ beforeEach(initialSetup)
 test('should create new profile', async () => {
     await request(app)
         .post('/api/profiles')
-        .set('Authorization', userOne.tokens[0].token)
+        .set('Authorization', userTwo.tokens[0].token)
         .send({
             first_name: 'Vencee',
             last_name: 'Manansala'
@@ -24,5 +24,37 @@ test('should not create new profile', async () => {
             first_name: 'Vencee',
             last_name: 'Manansala'
         })
+        .expect(401)
+})
+
+/**
+ * Should not create new profile when there is already created
+ * profile for that user
+ */
+test('should not create new profile when there is already created', async () => {
+    await request(app)
+        .post('/api/profiles')
+        .set('Authorization', userOne.tokens[0].token)
+        .send({
+            first_name: 'Vencee',
+            last_name: 'Manansala'
+        })
+        .expect(400)
+})
+
+/** Should get the user profile */
+test('should get user profile', async () => {
+    await request(app)
+        .get('/api/profiles')
+        .set('Authorization', userOne.tokens[0].token)
+        .send()
+        .expect(200)
+})
+
+/** Should not get the user profile when no login user */
+test('should not get user profile when no login user', async () => {
+    await request(app)
+        .get('/api/profiles')
+        .send()
         .expect(401)
 })
