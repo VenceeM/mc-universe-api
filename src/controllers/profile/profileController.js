21182ed0bf7e0f3e03aafed7c155b95466d8ae96
officialvencee <officialvencee.manansala@gmail.com>
2022-01-11 22:54:34 +0800

    create a function to update user profile

--- src/controllers/profile/profileController.js
+++ src/controllers/profile/profileController.js
@@ -34,7 +34,36 @@ const getProfile = async (req, res) => {
     }
 }
 
+/** Update User profile */
+const updateProfile = async (req, res) => {
+    const fields = Object.keys(req.body)
+    const allowedField = ['first_name', 'last_name', 'avatar', 'favorite_hero', 'favorite_hero_img', 'profile_bg']
+    const isFieldAllowed = fields.every((field) => {
+        return allowedField.includes(field)
+    })
+
+    if (!isFieldAllowed) return res.status(400).send()
+
+    try {
+        const profile = await Profile.findOne({ user_owner: req.user._id })
+        if (!profile) return res.status(404).send()
+
+        fields.forEach((field) => {
+            profile[field] = req.body[field]
+        })
+
+        await profile.save()
+
+        res.status(200).send(profile)
+
+    } catch (e) {
+        res.status(500).send()
+    }
+}
+
 module.exports = {
     create: createProfile,
-    getProfile: getProfile
+    getProfile: getProfile,
+    updateProfile: updateProfile
+
 }
